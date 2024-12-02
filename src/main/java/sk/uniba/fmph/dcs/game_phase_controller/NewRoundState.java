69a4a1c953b7b759a3dad7012107d6c9739fc809
fenkaiser <fenkysadze@gmail.com>
2024-12-02 20:16:41 +0100

    Add files via upload

---
+++ src/main/java/sk/uniba/fmph/dcs/game_phase_controller/NewRoundState.java
@@ -0,0 +1,59 @@
+package sk.uniba.fmph.dcs.game_phase_controller;
+
+import sk.uniba.fmph.dcs.stone_age.*;
+import java.util.Collection;
+
+public class NewRoundState implements InterfaceGamePhaseState {
+    private final InterfaceNewTurn newTurnHandler;
+
+    public NewRoundState(InterfaceNewTurn newTurnHandler) {
+        this.newTurnHandler = newTurnHandler;
+    }
+
+    @Override
+    public HasAction tryToMakeAutomaticAction(PlayerOrder player) {
+        newTurnHandler.newTurn();
+        return HasAction.AUTOMATIC_ACTION_DONE;
+    }
+
+    // don't do in this phase:
+    @Override
+    public ActionResult placeFigures(PlayerOrder player, Location location, int figuresCount) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult makeAction(PlayerOrder player, Location location, Collection<Effect> inputResources, Collection<Effect> outputResources) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult skipAction(PlayerOrder player, Location location) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult useTools(PlayerOrder player, int toolIndex) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult noMoreToolsThisThrow(PlayerOrder player) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult feedTribe(PlayerOrder player, Collection<Effect> resources) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult doNotFeedThisTurn(PlayerOrder player) {
+        return ActionResult.FAILURE;
+    }
+
+    @Override
+    public ActionResult makeAllPlayersTakeARewardChoice(PlayerOrder player, Effect reward) {
+        return ActionResult.FAILURE;
+    }
+}
